c71b54e3d978e4304949e496f1149e9ea905d275
ArtekII <216646542+ArtekII@users.noreply.github.com>
2026-06-23 11:49:20 +0300

    S2

---
+++ src/pumpkin/annotation/UrlMapping.java
@@ -0,0 +1,13 @@
+package pumpkin.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD) // S'applique uniquement sur les méthodes
+
+public @interface UrlMapping {
+    String value() default "";
+}
---
+++ src/pumpkin/mapping/Mapping.java
@@ -0,0 +1,33 @@
+package pumpkin.mapping;
+
+public class Mapping {
+    // il faut assuer l'uniciter de l'url, quand on fait /liste, il faut pas qu'il y ait un autre /liste
+    private String nomClasse;
+    private String nomMethode;
+
+    public Mapping(String nomClasse, String nomMethode) {
+        this.nomClasse = nomClasse;
+        this.nomMethode = nomMethode;
+    }
+
+    public String getNomClasse() {
+        return nomClasse;
+    }
+    public void setNomClasse(String nomClasse) {
+        this.nomClasse = nomClasse;
+    }
+    public String getNomMethode() {
+        return nomMethode;
+    }
+    public void setNomMethode(String nomMethode) {
+        this.nomMethode = nomMethode;
+    }
+
+    @Override
+    public String toString() {
+        return "Mapping{" +
+            "nomClasse='" + nomClasse + '\'' +
+            ", nomMethode='" + nomMethode + '\'' +
+            '}';
+    }
+}
